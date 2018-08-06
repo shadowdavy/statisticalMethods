e388f2fddd575cf39901fb774bc1c1f1d99b9400
David <shadowdavy@gmail.com>
2018-08-06 15:34:04 +0200

    comments in code

--- monday06082018/exercise05/exercise05.r
+++ monday06082018/exercise05/exercise05.r
@@ -1,23 +1,31 @@
 primeNum <- function(n){
-    # initialise empty prime vector
+    # initialise empty prime vector (note vector is NULL not really empty)
     prime <- c()
-    # one is not a prime number
+    # one is not a prime number return message
     if(n == 1) {
-        print("one is not a prime number")
-        break
+        return("one is not a prime number")
     } else {
-        # number is prime until proofen wrong
-        for(ifPrime in 2:n) {
+        # if n is greater than one
+        # for improvement we can check for prime numbers between a range of numbers
+        # therefore we can commit, instead of a value n, two numbers lowerN and upperN
+        # after that we can set the for loop instead in range of 2:n to lowerN:upperN
+        for(num in 2:n) {
+            # number is prime until proofen wrong
             isPrime = TRUE
-            for(i in 2:(ifPrime - 1)) {
-                if (ifPrime == 2) {
+            for(i in 2:(num - 1)) {
+                # if number is two continue because two is prime number
+                if (num == 2) {
                     isPrime = TRUE
-                } else if ((ifPrime %% i) == 0) {
+                } else if ((num %% i) == 0) {
+                    # if number can be divided by any other number set
+                    # isPrime to false, because than the checked number is no
+                    # prime number
                     isPrime = FALSE
                 }
             }
             if(isPrime) {
-                prime <- c(prime, ifPrime)
+                # append prime vector with the cecked prime numbers
+                prime <- c(prime, num)
             }
         }
     }
